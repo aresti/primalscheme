da00e44beeed4a6363a348e520d6c58cc66c3b33
joshquick <j.quick@bham.ac.uk>
2016-09-28 08:46:12 +0100

    Implemented proper retry logic Fix issue where last region was too short

--- scheme.py
+++ scheme.py
@@ -1,6 +1,5 @@
 import sys
 from Bio import SeqIO
-from Bio import Seq
 import primer3
 from pprint import pprint
 import settings
@@ -8,43 +7,81 @@ import interactions
 import pairwise
 from primal_models import Region
 from operator import itemgetter
+import re
+
+class Explain():
+
+	def __init__(self, string):
+		self.considered = 0
+		self.GC_content_failed = 0
+		self.low_tm = 0
+		self.high_tm = 0
+		self.not_in_any_ok_left_region = 0
+		self.not_in_any_ok_right_region = 0
+		self.ok = 0
+		self.explain(string)
+
+	def explain(self, string):
+		fields = ['considered', 'GC content failed', 'low tm', 'high tm', 'not in any ok left region', 'not in any ok right region', 'ok']
+		for field in fields:
+			pattern = field.replace(' ', '\s') + '\s(\d+)'
+			matches = re.search(pattern, string)
+			if matches:
+				setattr(self, field.replace('\s','_'), int(matches.group(1)))
 
 def run(args):
 	#Use first record for primer picking
-	references = []
-	dna_bases = 'ACGT'
-	for reference in SeqIO.parse(open(args.g, 'r'), 'fasta'):
-		reference.seq = Seq.Seq(''.join([base if base in dna_bases else 'N' for base in str(reference.seq).replace('U', 'T')]))
-		references.append(reference)
+	references = list(SeqIO.parse(open(args.g, 'r'), 'fasta'))
 	seq = str(references[0].seq)
-	step = args.length - 2*(args.overlap)
+	regions, remain = divmod(len(seq)-(2*args.overlap), args.length-(2*args.overlap))
+	#substract the overlaps from the ends then divide the rest by the non-overlapping region 
+	step = args.length-(2*args.overlap) + int(round(remain/float(regions)))
+	#divide the remainder across the regions
+	print 'Reference size %i, step size %i, regions %i, remain %i' %(len(seq), step, regions, remain)
 	seq_params = {
 		     'SEQUENCE_ID': 'blah',
 		     'SEQUENCE_TEMPLATE': seq,
 		     'SEQUENCE_INCLUDED_REGION': [0, len(seq)],
 		     }
-	return references, seq, step, seq_params 
+	return references, seq, step, seq_params, regions 
 
 def multiplex(args, parser=None):
-	references, seq, step, seq_params = run(args)
+	references, seq, step, seq_params, regions = run(args)
 	outer_params = settings.outer_pair(args)
 	result = []
+	key = 'SEQUENCE_PRIMER_PAIR_OK_REGION_LIST'
 	print 'Running primer3'
-	for i, region in enumerate(range(args.length/2, len(seq), step)):
-		#sys.stdout.write('.')
-		#sys.stdout.flush()
-		seq_params['SEQUENCE_PRIMER_PAIR_OK_REGION_LIST'] = [region-(args.length/2), args.overlap, region+(args.length/2)-args.overlap, args.overlap]
-		if region > len(seq)-step:
-			seq_params['SEQUENCE_PRIMER_PAIR_OK_REGION_LIST'] = [region-200, args.overlap, len(seq)-50, 50]
+	for i, region in enumerate(range(0, len(seq), step)[:-1]):
+		print 'Region %i, position %i' %(i+1, region)
+		left_start = region
+		left_end = right_end = args.overlap
+		right_start = region+args.length-args.overlap
+		if i+1 == regions:
+			right_end = 25
+			right_start = len(seq)-right_end
+		for j in range(4):
+			seq_params[key] = [left_start, left_end, right_start, right_end]
+			print seq_params[key]
 			output = primer3.bindings.designPrimers(seq_params, outer_params)
-		else:
-			output = primer3.bindings.designPrimers(seq_params, outer_params)
-			try:
-				output['PRIMER_LEFT_0']
-				output['PRIMER_RIGHT_0']
-			except KeyError:
-				seq_params['SEQUENCE_PRIMER_PAIR_OK_REGION_LIST'] = [region-(args.length/2), args.overlap+25, region+(args.length/2)-(args.overlap+25), args.overlap+25]
-				output = primer3.bindings.designPrimers(seq_params, outer_params)
+			left_ok = Explain(output['PRIMER_LEFT_EXPLAIN']).ok
+			right_ok = Explain(output['PRIMER_RIGHT_EXPLAIN']).ok
+			if left_ok < 3:
+				print 'Found %i left and %i right primers on iteration %i' %(left_ok, right_ok, j+1)
+				left_start -= 10
+				left_end += 10
+			elif right_ok < 3:
+				print 'Found %i left and %i right primers on iteration %i' %(left_ok, right_ok, j+1)
+				if i+1 == regions:
+					#Work back from right for last region
+					right_end += 10
+					right_start = len(seq)-right_end
+				else:
+					right_start -= 10
+					right_end += 10 
+			else:
+				print 'Found %i left and %i right primers on iteration %i' %(left_ok, right_ok, j+1)
+				break
+		
 
 		region = Region(args.length, i+1, output, references)
 		result.append(region)
