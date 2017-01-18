12c2df810557784294f40f71caea3b4f35395914
Andy Smith <andysmith.d@gmail.com>
2017-01-18 10:03:21 +0000

    Improve diagram image quality

--- primal/plot.py
+++ primal/plot.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from Bio import SeqIO
@@ -8,10 +9,9 @@ from Bio.Graphics import GenomeDiagram
 from Bio.SeqFeature import SeqFeature, FeatureLocation
 
 
-def plot_schemeadelica(filename, reference, results):
+def plot_schemeadelica(prefix, results, reference, path='./'):
 	gd_diagram = GenomeDiagram.Diagram("Primer Scheme", track_size=1)
-
-	scale_track = GenomeDiagram.Track(name='scale', scale=True, scale_largetick_interval=1000, height=0.1)
+	scale_track = GenomeDiagram.Track(name='scale', scale=True, scale_fontsize=10, scale_largetick_interval=1000, height=0.1)
 	gd_diagram.add_track(scale_track, 2)
 
 	primer_feature_set_1 = GenomeDiagram.FeatureSet()
@@ -27,11 +27,11 @@ def plot_schemeadelica(filename, reference, results):
 		rev_feature = SeqFeature(FeatureLocation(int(cols2[1]), int(cols2[2]), strand=0))
 		region_feature = SeqFeature(FeatureLocation(int(cols1[1]), int(cols2[2]), strand=0))
 		if int(region) % 2 == 0:
-			primer_feature_set_1.add_feature(region_feature, color=colors.palevioletred, name=region, label=True, label_size = 6, label_position="middle", label_angle = 0)
+			primer_feature_set_1.add_feature(region_feature, color=colors.palevioletred, name=region, label=True, label_size=10, label_position="middle", label_angle = 0)
 			primer_feature_set_1.add_feature(fwd_feature, color=colors.red, name=region, label=False)
 			primer_feature_set_1.add_feature(rev_feature, color=colors.red, name=region, label=False)
 		else:
-			primer_feature_set_2.add_feature(region_feature, color=colors.palevioletred, name=region, label=True, label_size = 6, label_position="middle", label_angle = 0)
+			primer_feature_set_2.add_feature(region_feature, color=colors.palevioletred, name=region, label=True, label_size=10, label_position="middle", label_angle = 0)
 			primer_feature_set_2.add_feature(fwd_feature, color=colors.red, name=region, label=False)
 			primer_feature_set_2.add_feature(rev_feature, color=colors.red, name=region, label=False)
 
@@ -44,8 +44,13 @@ def plot_schemeadelica(filename, reference, results):
 	primer_track.add_set(primer_feature_set_2)
 	gd_diagram.add_track(primer_track, 6)
 
+	rows = int(round(len(reference)/5000))
+	print dir(gd_diagram)
+	gd_diagram.draw(format='linear', pagesize=(1000, 250 * rows), fragments=rows, start=0, end=len(reference))
 
-	gd_diagram.draw(format='linear', pagesize=(800, 200), fragments=2, start=0, end=len(reference))
-
-	gd_diagram.write(filename + '.pdf', "pdf")
-	gd_diagram.write(filename + '.png', 'PNG')
+	png_filepath = os.path.join(path, '{}.png'.format(prefix))
+	pdf_filepath = os.path.join(path, '{}.pdf'.format(prefix))
+	svg_filepath = os.path.join(path, '{}.svg'.format(prefix))
+	gd_diagram.write(png_filepath, 'PNG', dpi=300)
+	gd_diagram.write(pdf_filepath, 'PDF', dpi=300)
+	gd_diagram.write(svg_filepath, 'SVG', dpi=300)
