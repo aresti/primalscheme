3694099583662bcd017b2510730cfc0c360edaa4
Josh Quick <josh@COLLES-12299.local>
2017-07-09 23:17:17 +0100

    Add currently unused barcodes to settings file

--- primal/clustal_test.py
+++
@@ -1,8 +0,0 @@
-from Bio.Align.Applications import ClustalOmegaCommandline
-import sys
-
-in_file = sys.argv[1]
-out_file = in_file + '.align'
-clustalomega_cline = ClustalOmegaCommandline(infile=in_file, outfile=out_file, verbose=True, percentid=True, distmat_out=in_file + '.dist', distmat_full=True)
-print clustalomega_cline
-clustalomega_cline()
