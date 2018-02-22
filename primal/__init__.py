8da4d60ddbbb7bee091b5d67cfd758de26f51387
Josh Quick <josh@BIO1188.BHAM.AC.UK>
2018-02-22 17:07:45 +0000

    Beautify

--- primal/__init__.py
+++ primal/__init__.py
@@ -10,173 +10,208 @@ from plot import plot_schemeadelica
 
 
 class PoolOverlapException(Exception):
-	"""Ran into previous primer in this pool"""
-	pass
+    """Ran into previous region in this pool"""
+    pass
+
+
+class SchemeOverlapException(Exception):
+    """Ran into previous region in other pool"""
+    pass
+
 
 class NoSuitableException(Exception):
-	"""No suitable primer found"""
-	pass
+    """No suitable primer found"""
+    pass
 
 
 def find_primers(prefix, logger, amplicon_length, min_overlap, search_space, max_candidates, references, region_num, start_limits, debug):
-	"""
-	Given a list of biopython SeqRecords (references), and a string representation
-	of the pimary reference (seq), return a list of Region objects containing candidate
-	primer pairs sorted by an alignment score summed over all references.
-	"""
+    """
+    Given a list of biopython SeqRecords (references), and a string representation
+    of the pimary reference (seq), return a list of Region objects containing candidate
+    primer pairs sorted by an alignment score summed over all references.
+    """
 
-	# Slice primary reference to speed up Primer3 on long sequences
-	chunk_end = 2 * amplicon_length if region_num == 1 else start_limits[1] + amplicon_length
-	seq = str(references[0].seq)[start_limits[0]:chunk_end]
+    # Slice primary reference to speed up Primer3 on long sequences
+    chunk_end = 2 * \
+        amplicon_length if region_num == 1 else start_limits[1] + \
+        amplicon_length
+    seq = str(references[0].seq)[start_limits[0]:chunk_end]
 
-	# Primer3 setup
-	p3_global_args = settings.outer_params
-	region_key = 'SEQUENCE_PRIMER_PAIR_OK_REGION_LIST'
-	start = 0 if region_num == 1 else start_limits[1] - start_limits[0] - search_space
-	p3_seq_args = {
-		region_key: [min(start, len(seq) - amplicon_length), search_space, -1, -1],
-		'SEQUENCE_TEMPLATE': seq,
-		'SEQUENCE_INCLUDED_REGION': [0, len(seq) - 1]
-	}
+    # Primer3 setup
+    p3_global_args = settings.outer_params
+    region_key = 'SEQUENCE_PRIMER_PAIR_OK_REGION_LIST'
+    start = 0 if region_num == 1 else start_limits[1] - \
+        start_limits[0] - search_space
+    p3_seq_args = {
+        region_key: [min(start, len(seq) - amplicon_length), search_space, -1, -1],
+        'SEQUENCE_TEMPLATE': seq,
+        'SEQUENCE_INCLUDED_REGION': [0, len(seq) - 1]
+    }
 
+    allowed_variation = amplicon_length * 0.1
+    p3_global_args['PRIMER_PRODUCT_SIZE_RANGE'] = [
+        [amplicon_length - allowed_variation, amplicon_length + allowed_variation]]
+    p3_global_args['PRIMER_NUM_RETURN'] = max_candidates
 
-	allowed_variation = amplicon_length * 0.1
-	p3_global_args['PRIMER_PRODUCT_SIZE_RANGE'] = [[amplicon_length - allowed_variation, amplicon_length + allowed_variation]]
-	p3_global_args['PRIMER_NUM_RETURN'] = max_candidates
+    while True:
+        if debug:
+            pass
+            logger.debug('Primer3 Settings:')
+            logger.debug(p3_seq_args)
+            logger.debug(p3_global_args)
 
+        primer3_output = primer3.bindings.designPrimers(
+            p3_seq_args, p3_global_args)
+        # logger.debug(vars(primer3_output))
 
-	while True:
-		if debug:
-			pass
-			logger.debug('Primer3 Settings:')
-			logger.debug(p3_seq_args)
-			logger.debug(p3_global_args)
+        num_pairs_returned = primer3_output['PRIMER_PAIR_NUM_RETURNED']
+        if num_pairs_returned:
+            break
 
-		primer3_output = primer3.bindings.designPrimers(p3_seq_args, p3_global_args)
-		#logger.debug(vars(primer3_output))
+        # Step right only if no primers in region 1
+        if region_num == 1:
+            p3_seq_args[region_key][1] += settings.STEP_DISTANCE
+            logger.info("Stepping right, position %i, limit %s" %
+                        (p3_seq_args[region_key][1], 'none'))
+        # Step left (increase overlap)
+        else:
+            p3_seq_args[region_key][0] -= settings.STEP_DISTANCE
+            p3_seq_args[region_key][1] += settings.STEP_DISTANCE
+            logger.info("Stepping left, position %i, limit %i" % (
+                p3_seq_args[region_key][0] + start_limits[0], start_limits[0]))
 
-		num_pairs_returned = primer3_output['PRIMER_PAIR_NUM_RETURNED']
-		if num_pairs_returned:
-			break
+        # Check if we've run into the left limit
+        if p3_seq_args[region_key][0] < 0:
+            raise NoSuitableException(
+                "No suitable primers found for region {} with current parameters. Try increasing amplicon length or reducing overlap.".format(region_num))
 
-		# Step right only if no primers in region 1
-		if region_num == 1:
-			p3_seq_args[region_key][1] += settings.STEP_DISTANCE
-			logger.info("Stepping right, position %i, limit %s" %(p3_seq_args[region_key][1], 'none'))
-		# Step left (increase overlap)
-		else:
-			p3_seq_args[region_key][0] -= settings.STEP_DISTANCE
-			p3_seq_args[region_key][1] += settings.STEP_DISTANCE
-			logger.info("Stepping left, position %i, limit %i" %(p3_seq_args[region_key][0] + start_limits[0], start_limits[0]))
-
-		# Check if we've run into the left limit
-		if p3_seq_args[region_key][0] < 0:
-			raise NoSuitableException("No suitable primers found for region {} with current parameters. Try increasing amplicon length or reducing overlap.".format(region_num))
-
-	#print start_limits
-	return Region(prefix, region_num, max_candidates, start_limits, primer3_output, references)
+    # print start_limits
+    return Region(prefix, region_num, max_candidates, start_limits, primer3_output, references)
 
 
 def write_bed(prefix, results, reference_id, path='./'):
-	filepath = os.path.join(path, '{}.bed'.format(prefix))
-	with open(filepath, 'w') as bedhandle:
-		for r in results:
-			print >>bedhandle, '\t'.join(map(str, [reference_id, r.candidate_pairs[0].left.start, r.candidate_pairs[0].left.end, r.candidate_pairs[0].left.name, r.pool]))
-			print >>bedhandle, '\t'.join(map(str, [reference_id, r.candidate_pairs[0].right.end, r.candidate_pairs[0].right.start, r.candidate_pairs[0].right.name, r.pool]))
+    filepath = os.path.join(path, '{}.bed'.format(prefix))
+    with open(filepath, 'w') as bedhandle:
+        for r in results:
+            print >>bedhandle, '\t'.join(map(
+                str, [reference_id, r.candidate_pairs[0].left.start, r.candidate_pairs[0].left.end, r.candidate_pairs[0].left.name, r.pool]))
+            print >>bedhandle, '\t'.join(map(str, [reference_id, r.candidate_pairs[0].right.end,
+                                                   r.candidate_pairs[0].right.start, r.candidate_pairs[0].right.name, r.pool]))
 
 
 def write_tsv(prefix, results, path='./'):
-	filepath = os.path.join(path, '{}.tsv'.format(prefix))
-	with open(filepath, 'w') as tsvhandle:
-		print >>tsvhandle, '\t'.join(['name', 'seq', 'length', '%gc', 'tm (use 65)'])
-		for r in results:
-			left = r.candidate_pairs[0].left
-			right = r.candidate_pairs[0].right
-			print >>tsvhandle, '\t'.join(map(str, [left.name, left.seq, left.length, left.gc, left.tm]))
-			print >>tsvhandle, '\t'.join(map(str, [right.name, right.seq, right.length, right.gc, right.tm]))
+    filepath = os.path.join(path, '{}.tsv'.format(prefix))
+    with open(filepath, 'w') as tsvhandle:
+        print >>tsvhandle, '\t'.join(
+            ['name', 'seq', 'length', '%gc', 'tm (use 65)'])
+        for r in results:
+            left = r.candidate_pairs[0].left
+            right = r.candidate_pairs[0].right
+            print >>tsvhandle, '\t'.join(
+                map(str, [left.name, left.seq, left.length, left.gc, left.tm]))
+            print >>tsvhandle, '\t'.join(
+                map(str, [right.name, right.seq, right.length, right.gc, right.tm]))
+
 
 def write_refs(prefix, refs, path='./'):
-	filepath = os.path.join(path, '{}.fasta'.format(prefix))
-	with open(filepath, 'w') as refhandle:
-		SeqIO.write(refs, filepath, 'fasta')
+    filepath = os.path.join(path, '{}.fasta'.format(prefix))
+    with open(filepath, 'w') as refhandle:
+        SeqIO.write(refs, filepath, 'fasta')
+
 
 def multiplex(args, logger, parser=None):
-	# Check for sensible parameters
-	if args.amplicon_length < 100 or args.amplicon_length > 2000:
-		logger.error("--amplicon-length set to %s, must be within the range 100 to 2000" %(args.amplicon_length))
-		raise ValueError("--amplicon-length set to %s, must be within the range 100 to 2000" %(args.amplicon_length))
+    # Check for sensible parameters
+    if args.amplicon_length < 100 or args.amplicon_length > 2000:
+        logger.error(
+            "--amplicon-length set to %s, must be within the range 100 to 2000" % (args.amplicon_length))
+        raise ValueError(
+            "--amplicon-length set to %s, must be within the range 100 to 2000" % (args.amplicon_length))
 
-	references = args.references
-	results =[]
-	region_num = 0
-	window_size = 50
+    references = args.references
+    results = []
+    region_num = 0
+    window_size = 50
 
-	# Check there are enough regions to allow limits and end logic to work
-	if len(references[0]) < 2 * args.amplicon_length:
-		# This needs validating
-		logger.error("Reference length %s, must be twice amplicon length" %(len(references[0])))
-		raise ValueError("Reference length %s, must be twice amplicon length" %(len(references[0])))
+    # Check there are enough regions to allow limits and end logic to work
+    if len(references[0]) < 2 * args.amplicon_length:
+        # This needs validating
+        logger.error("Reference length %s, must be twice amplicon length" % (
+            len(references[0])))
+        raise ValueError(
+            "Reference length %s, must be twice amplicon length" % (len(references[0])))
 
-	while True:
-		region_num += 1
+    while True:
+        region_num += 1
 
-		# Left limit prevents crashing into the previous primer in this pool
-		left_start_limit = results[-2].candidate_pairs[0].right.start + 1 if region_num > 2 else 0
+        # Left limit prevents crashing into the previous primer in this pool
+        left_start_limit = results[-2].candidate_pairs[0].right.start + \
+            1 if region_num > 2 else 0
 
-		# Right limit maintains a minimum overlap of 0 (no gap)
-		right_start_limit = results[-1].candidate_pairs[0].right.end - args.min_overlap - 1 if region_num > 1 else 0
+        # Right limit maintains a minimum overlap of 0 (no gap)
+        right_start_limit = results[-1].candidate_pairs[0].right.end - \
+            args.min_overlap - 1 if region_num > 1 else 0
 
-		# Find primers for this region
-		is_last_region = (region_num > 1 and len(references[0]) - results[-1].candidate_pairs[0].right.start < args.amplicon_length)
-		try:
-			region = find_primers(args.prefix, logger, args.amplicon_length, args.min_overlap, args.search_space, args.max_candidates, references, region_num, (left_start_limit, right_start_limit), args.debug)
-		except PoolOverlapException("Ran into previous region in pool") as e:
-			if not is_last_region:
-				logger.error("Ran into previous region in pool")
-				raise e
+        # Find primers for this region
+        is_last_region = (region_num > 1 and len(
+            references[0]) - results[-1].candidate_pairs[0].right.start < args.amplicon_length)
+        try:
+            region = find_primers(args.prefix, logger, args.amplicon_length, args.min_overlap, args.search_space,
+                                  args.max_candidates, references, region_num, (left_start_limit, right_start_limit), args.debug)
+        except PoolOverlapException("Ran into previous region in pool") as e:
+            if not is_last_region:
+                logger.error("Ran into previous region in pool")
+                raise e
 
-		if args.debug:
-			num = len(region.candidate_pairs[0].left.alignments)
-			for i in range(num):
-				logger.debug(region.candidate_pairs[0].left.alignments[i].formatted_alignment)
-				logger.debug('Score:%i' %(region.candidate_pairs[0].left.alignments[i].score))
-				logger.debug(region.candidate_pairs[0].right.alignments[i].formatted_alignment)
-				logger.debug('Score:%i' %(region.candidate_pairs[0].right.alignments[i].score))
+        if args.debug:
+            num = len(region.candidate_pairs[0].left.alignments)
+            for i in range(num):
+                logger.debug(
+                    region.candidate_pairs[0].left.alignments[i].formatted_alignment)
+                logger.debug('Score:%i' % (
+                    region.candidate_pairs[0].left.alignments[i].score))
+                logger.debug(
+                    region.candidate_pairs[0].right.alignments[i].formatted_alignment)
+                logger.debug('Score:%i' % (
+                    region.candidate_pairs[0].right.alignments[i].score))
 
-		results.append(region)
+        results.append(region)
 
-		# Reporting
-		logger.info("Region %i, %i:%i" %(region_num, results[-1].candidate_pairs[0].left.start, results[-1].candidate_pairs[0].right.start))
-		if region_num > 1:
-			# Remember, results now include this one, so -2 is the other pool
-			trimmed_overlap = results[-2].candidate_pairs[0].right.end - results[-1].candidate_pairs[0].left.end - 1
-			logger.info("Product length %i, trimmed overlap %i" % (results[-1].candidate_pairs[0].product_length, trimmed_overlap))
-		else:
-			logger.info("Product length %i" % (results[-1].candidate_pairs[0].product_length))
-		if args.debug:
-			logger.debug(vars(results[-1].candidate_pairs[0].left))
-			logger.debug(vars(results[-1].candidate_pairs[0].right))
+        # Reporting
+        logger.info("Region %i, %i:%i" % (
+            region_num, results[-1].candidate_pairs[0].left.start, results[-1].candidate_pairs[0].right.start))
+        if region_num > 1:
+            # Remember, results now include this one, so -2 is the other pool
+            trimmed_overlap = results[-2].candidate_pairs[0].right.end - \
+                results[-1].candidate_pairs[0].left.end - 1
+            logger.info("Product length %i, trimmed overlap %i" % (
+                results[-1].candidate_pairs[0].product_length, trimmed_overlap))
+        else:
+            logger.info("Product length %i" %
+                        (results[-1].candidate_pairs[0].product_length))
+        if args.debug:
+            logger.debug(vars(results[-1].candidate_pairs[0].left))
+            logger.debug(vars(results[-1].candidate_pairs[0].right))
 
-		# Handle the end so maximum uncovered genome is one overlaps length
-		if is_last_region:
-			break
+        # Handle the end so maximum uncovered genome is one overlaps length
+        if is_last_region:
+            break
 
+    # Write bed and image files
+    if os.path.isdir(args.output_path) and not args.force:
+        logger.error('Directory exists add --force to overwrite')
+        raise IOError('Directory exists add --force to overwrite')
+        sys.exit()
+    if not os.path.isdir(args.output_path):
+        os.mkdir(args.output_path)
+    write_bed(args.prefix, results, references[0].id, args.output_path)
+    logger.info('BED written')
+    write_tsv(args.prefix, results, args.output_path)
+    logger.info('TSV written')
+    write_refs(args.prefix, references, args.output_path)
+    logger.info('FASTA written')
+    plot_schemeadelica(args.prefix, logger, results,
+                       references[0], args.output_path)
+    logger.info('PDF written')
+    logger.info('Complete')
 
-	# Write bed and image files
-	if os.path.isdir(args.output_path) and not args.force:
-		logger.error('Directory exists add --force to overwrite')
-		raise IOError('Directory exists add --force to overwrite')
-		sys.exit()
-	if not os.path.isdir(args.output_path):
-		os.mkdir(args.output_path)
-	write_bed(args.prefix, results, references[0].id, args.output_path)
-	logger.info('BED written')
-	write_tsv(args.prefix, results, args.output_path)
-	logger.info('TSV written')
-	write_refs(args.prefix, references, args.output_path)
-	logger.info('FASTA written')
-	plot_schemeadelica(args.prefix, logger, results, references[0], args.output_path)
-	logger.info('PDF written')
-	logger.info('Complete')
-
-	return results
+    return results
--- primal/plot.py
+++ primal/plot.py
@@ -10,44 +10,56 @@ from Bio.SeqFeature import SeqFeature, FeatureLocation
 
 
 def plot_schemeadelica(prefix, logger, results, reference, path='./'):
-	gd_diagram = GenomeDiagram.Diagram("Primer Scheme", track_size=1)
-	scale_track = GenomeDiagram.Track(name='scale', scale=True, scale_fontsize=10, scale_largetick_interval=1000, height=0.1)
-	gd_diagram.add_track(scale_track, 2)
+    gd_diagram = GenomeDiagram.Diagram("Primer Scheme", track_size=1)
+    scale_track = GenomeDiagram.Track(
+        name='scale', scale=True, scale_fontsize=10, scale_largetick_interval=1000, height=0.1)
+    gd_diagram.add_track(scale_track, 2)
 
-	primer_feature_set_1 = GenomeDiagram.FeatureSet()
-	primer_feature_set_2 = GenomeDiagram.FeatureSet()
+    primer_feature_set_1 = GenomeDiagram.FeatureSet()
+    primer_feature_set_2 = GenomeDiagram.FeatureSet()
 
-	for r in results:
-		cols1 = [reference.id, r.candidate_pairs[0].left.start, r.candidate_pairs[0].left.end, r.candidate_pairs[0].left.name, r.pool]
-		cols2 = [reference.id, r.candidate_pairs[0].right.end, r.candidate_pairs[0].right.start, r.candidate_pairs[0].right.name, r.pool]
-		region = str(r.region_num)
-		fwd_feature = SeqFeature(FeatureLocation(int(cols1[1]), int(cols1[2]), strand=0))
-		rev_feature = SeqFeature(FeatureLocation(int(cols2[1]), int(cols2[2]), strand=0))
-		region_feature = SeqFeature(FeatureLocation(int(cols1[1]), int(cols2[2]), strand=0))
-		if int(region) % 2 == 0:
-			primer_feature_set_1.add_feature(region_feature, color=colors.palevioletred, name=region, label=True, label_size=10, label_position="middle", label_angle = 0)
-			primer_feature_set_1.add_feature(fwd_feature, color=colors.red, name=region, label=False)
-			primer_feature_set_1.add_feature(rev_feature, color=colors.red, name=region, label=False)
-		else:
-			primer_feature_set_2.add_feature(region_feature, color=colors.palevioletred, name=region, label=True, label_size=10, label_position="middle", label_angle = 0)
-			primer_feature_set_2.add_feature(fwd_feature, color=colors.red, name=region, label=False)
-			primer_feature_set_2.add_feature(rev_feature, color=colors.red, name=region, label=False)
+    for r in results:
+        cols1 = [reference.id, r.candidate_pairs[0].left.start,
+                 r.candidate_pairs[0].left.end, r.candidate_pairs[0].left.name, r.pool]
+        cols2 = [reference.id, r.candidate_pairs[0].right.end,
+                 r.candidate_pairs[0].right.start, r.candidate_pairs[0].right.name, r.pool]
+        region = str(r.region_num)
+        fwd_feature = SeqFeature(FeatureLocation(
+            int(cols1[1]), int(cols1[2]), strand=0))
+        rev_feature = SeqFeature(FeatureLocation(
+            int(cols2[1]), int(cols2[2]), strand=0))
+        region_feature = SeqFeature(FeatureLocation(
+            int(cols1[1]), int(cols2[2]), strand=0))
+        if int(region) % 2 == 0:
+            primer_feature_set_1.add_feature(region_feature, color=colors.palevioletred,
+                                             name=region, label=True, label_size=10, label_position="middle", label_angle=0)
+            primer_feature_set_1.add_feature(
+                fwd_feature, color=colors.red, name=region, label=False)
+            primer_feature_set_1.add_feature(
+                rev_feature, color=colors.red, name=region, label=False)
+        else:
+            primer_feature_set_2.add_feature(region_feature, color=colors.palevioletred,
+                                             name=region, label=True, label_size=10, label_position="middle", label_angle=0)
+            primer_feature_set_2.add_feature(
+                fwd_feature, color=colors.red, name=region, label=False)
+            primer_feature_set_2.add_feature(
+                rev_feature, color=colors.red, name=region, label=False)
 
+    primer_track = GenomeDiagram.Track(name="Annotated Features", height=0.1)
+    primer_track.add_set(primer_feature_set_1)
+    gd_diagram.add_track(primer_track, 4)
 
-	primer_track = GenomeDiagram.Track(name="Annotated Features", height=0.1)
-	primer_track.add_set(primer_feature_set_1)
-	gd_diagram.add_track(primer_track, 4)
+    primer_track = GenomeDiagram.Track(name="Annotated Features", height=0.1)
+    primer_track.add_set(primer_feature_set_2)
+    gd_diagram.add_track(primer_track, 6)
 
-	primer_track = GenomeDiagram.Track(name="Annotated Features", height=0.1)
-	primer_track.add_set(primer_feature_set_2)
-	gd_diagram.add_track(primer_track, 6)
+    rows = max(1, int(round(len(reference) / 5000)))
+    gd_diagram.draw(format='linear', pagesize=(1000, 250 * rows),
+                    fragments=rows, start=0, end=len(reference))
 
-	rows = max(1, int(round(len(reference)/5000)))
-	gd_diagram.draw(format='linear', pagesize=(1000, 250 * rows), fragments=rows, start=0, end=len(reference))
-
-	png_filepath = os.path.join(path, '{}.png'.format(prefix))
-	pdf_filepath = os.path.join(path, '{}.pdf'.format(prefix))
-	svg_filepath = os.path.join(path, '{}.svg'.format(prefix))
-	gd_diagram.write(png_filepath, 'PNG', dpi=300)
-	gd_diagram.write(pdf_filepath, 'PDF', dpi=300)
-	gd_diagram.write(svg_filepath, 'SVG', dpi=300)
+    png_filepath = os.path.join(path, '{}.png'.format(prefix))
+    pdf_filepath = os.path.join(path, '{}.pdf'.format(prefix))
+    svg_filepath = os.path.join(path, '{}.svg'.format(prefix))
+    gd_diagram.write(png_filepath, 'PNG', dpi=300)
+    gd_diagram.write(pdf_filepath, 'PDF', dpi=300)
+    gd_diagram.write(svg_filepath, 'SVG', dpi=300)
