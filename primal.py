cc6d6d673f10079f7715c85d9a6d766035e81d48
Andrew Smith <andysmith.d@gmail.com>
2020-03-22 22:20:12 +0000

    Convenience wrapper

---
+++ primal.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Convenience wrapper for running PrimalScheme directly from source tree.
+"""
+
+from primalscheme.primalscheme import main
+
+
+if __name__ == '__main__':
+    main()
