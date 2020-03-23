f4b62a4ff40973ba047dd5e40aecaba030d3fb20
Andrew Smith <andysmith.d@gmail.com>
2020-03-23 14:56:54 +0000

    Fix route to main() in setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "primalscheme = primalscheme.primal:main",
+            "primalscheme = primalscheme.primalscheme:main",
         ],
     },
     classifiers=[
