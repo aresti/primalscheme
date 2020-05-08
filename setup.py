b5aa85fbe90610fd4f2459f541dcfeab7a3110a6
joshquick <j.quick@bham.ac.uk>
2020-05-08 17:53:29 +0100

    Bump version to 0.2.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='primalscheme',
-    version='0.2.4',
+    version='0.2.5',
     author='Josh Quick',
     author_email='j.quick@bham.ac.uk',
     license='GPL',
