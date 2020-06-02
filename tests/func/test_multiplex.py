631a94999cacf6a43eeb87b9256eb6b71bbd539f
Andrew Smith <andysmith.d@gmail.com>
2020-06-02 20:19:24 +0100

    Fix multiplex functional tests

--- tests/func/test_multiplex.py
+++ tests/func/test_multiplex.py
@@ -2,7 +2,7 @@ import pytest
 
 from Bio.Seq import Seq
 
-from primalscheme.cli import get_config, process_fasta
+from primalscheme.cli import process_fasta
 from primalscheme.multiplex import MultiplexScheme
 
 
@@ -11,18 +11,9 @@ def default_chikv_scheme(chikv_input):
     return get_scheme(chikv_input)
 
 
-def get_scheme(fasta, config=None):
-    if not config:
-        config = get_config()
+def get_scheme(fasta, **kwargs):
     references = process_fasta(fasta)
-    scheme = MultiplexScheme(
-        references,
-        config.get("primer3"),
-        config.get("target_overlap"),
-        config.get("step_distance"),
-        config.get("min_unique"),
-        config.get("prefix"),
-    )
+    scheme = MultiplexScheme(references, **kwargs)
     scheme.design_scheme()
     return scheme
 
@@ -33,19 +24,17 @@ def no_collisions(regions):
         pool = [r for r in regions if r.pool == pool_id]
         last_coord = -1
         for r in pool:
-            if r.top_pair.left.start <= last_coord:
+            if r.left.start <= last_coord:
                 return False
-            last_coord = r.top_pair.right.start
+            last_coord = r.right.start
     return True
 
 
 def test_chikv_scheme_has_no_gaps(default_chikv_scheme):
     regions = default_chikv_scheme.regions
-    inserts = [(r.top_pair.left.end + 1, r.top_pair.right.end) for r in regions]
+    inserts = [(r.left.end + 1, r.right.end) for r in regions]
     covered_coords = set([x for insert in inserts for x in range(*insert)])
-    all_coords = set(
-        range(regions[0].top_pair.left.end + 1, regions[-1].top_pair.right.end)
-    )
+    all_coords = set(range(regions[0].left.end + 1, regions[-1].right.end))
 
     assert all_coords.issubset(covered_coords)
 
@@ -56,48 +45,50 @@ def test_no_collisions_in_any_test_scheme(all_stored_inputs):
 
 
 @pytest.mark.parametrize("amplicon_size", range(200, 801, 200))
-def test_scheme_varying_amplicon_sizes(default_config, amplicon_size, chikv_input):
-    config = default_config
+def test_scheme_varying_amplicon_sizes(amplicon_size, chikv_input):
     variation = int(0.1 * amplicon_size / 2)
     amplicon_size_min = amplicon_size - variation
     amplicon_size_max = amplicon_size + variation
-    config["primer3"].update(
-        {"PRIMER_PRODUCT_SIZE_RANGE": [[amplicon_size_min, amplicon_size_max]]}
+    scheme = get_scheme(
+        chikv_input,
+        amplicon_size_min=amplicon_size_min,
+        amplicon_size_max=amplicon_size_max,
     )
-    scheme = get_scheme(chikv_input, config)
 
     regions = scheme.regions
     for region in regions[:4]:
-        product_size = region.top_pair.product_size
+        product_size = region.product_size
         assert product_size <= amplicon_size_max
         assert product_size >= amplicon_size_min
 
 
-def test_large_target_overlap_does_not_result_in_collision(default_config, chikv_input):
-    config = default_config.update({"target_overlap": 300})
-    scheme = get_scheme(chikv_input, config)
+def test_large_target_overlap_does_not_result_in_collision(chikv_input):
+    scheme = get_scheme(chikv_input, target_overlap=300)
 
     assert no_collisions(scheme.regions)
 
 
-def test_candidates_are_correctly_sorted(default_chikv_scheme):
+def test_left_candidates_are_correctly_sorted(default_chikv_scheme):
     region = default_chikv_scheme.regions[0]
-    identity = region.top_pair.mean_identity
-    penalty = region.top_pair.combined_penalty
-    for candidate in region.candidate_pairs:
-        if candidate.mean_identity > identity:
-            pytest.fail("Candidates are not sorted first by mean identity")
-        elif (
-            candidate.mean_identity == identity and candidate.combined_penalty < penalty
-        ):
-            pytest.fail("Candidates are not sorted second by penalty")
-        identity = candidate.mean_identity
+    penalty = region.left.combined_penalty
+    for candidate in region.left_candidates:
+        if candidate.combined_penalty < penalty:
+            pytest.fail("Candidates are not sorted by combined penalty")
+        penalty = candidate.combined_penalty
+
+
+def test_right_candidates_are_correctly_sorted(default_chikv_scheme):
+    region = default_chikv_scheme.regions[0]
+    penalty = region.right.combined_penalty
+    for candidate in region.right_candidates:
+        if candidate.combined_penalty < penalty:
+            pytest.fail("Candidates are not sorted by combined penalty")
         penalty = candidate.combined_penalty
 
 
 def test_left_primer_seq_matches_ref_slice(default_chikv_scheme):
     primary_ref = str(default_chikv_scheme.primary_ref.seq)
-    left = default_chikv_scheme.regions[0].top_pair.left
+    left = default_chikv_scheme.regions[0].left
     ref_slice = primary_ref[left.start : left.end + 1]
 
     assert len(left.seq) == left.size
@@ -106,7 +97,7 @@ def test_left_primer_seq_matches_ref_slice(default_chikv_scheme):
 
 def test_right_primer_seq_matches_ref_slice(default_chikv_scheme):
     primary_ref = str(default_chikv_scheme.primary_ref.seq)
-    right = default_chikv_scheme.regions[0].top_pair.right
+    right = default_chikv_scheme.regions[0].right
     ref_slice = str(Seq(primary_ref[right.end : right.start + 1]).reverse_complement())
 
     assert len(right.seq) == right.size
--- tests/unit/test_primer.py
+++ tests/unit/test_primer.py
@@ -4,32 +4,25 @@ from primalscheme import config
 from primalscheme.primer import Primer, Direction
 
 
-P3_GLOBAL = {
-    "PRIMER_NUM_RETURN": 10,
-    "PRIMER_PRODUCT_SIZE_RANGE": [[config.AMPLICON_SIZE_MIN, config.AMPLICON_SIZE_MIN]],
-    "PRIMER_OPT_SIZE": config.PRIMER_OPT_SIZE,
-    "PRIMER_MIN_SIZE": config.PRIMER_SIZE_MIN,
-    "PRIMER_MAX_SIZE": config.PRIMER_SIZE_MAX,
-    "PRIMER_OPT_TM": config.PRIMER_OPT_TM,
-    "PRIMER_MIN_TM": config.PRIMER_MIN_TM,
-    "PRIMER_MAX_TM": config.PRIMER_MAX_TM,
-    "PRIMER_MIN_GC": config.PRIMER_MIN_GC,
-    "PRIMER_MAX_GC": config.PRIMER_MAX_GC,
-    "PRIMER_MAX_POLY_X": config.PRIMER_MAX_HOMO,
-    "PRIMER_SALT_MONOVALENT": config.MV_CONC,
-    "PRIMER_DNA_CONC": 50,
-    "PRIMER_MAX_NS_ACCEPTED": 0,
-    "PRIMER_MAX_SELF_ANY_TH": 47,
-    "PRIMER_MAX_SELF_END_TH": 47,
-    "PRIMER_PAIR_MAX_COMPL_ANY_TH": 47,
-    "PRIMER_PAIR_MAX_COMPL_END_TH": 47,
-    "PRIMER_MAX_HAIRPIN_TH": 47,
-    "PRIMER_PICK_INTERNAL_OLIGO": 0,
-}
-
-
 def test_base_penalty_matches_primer3_penalty(random_reference_slice):
     """Check internal penalty calculation matches primer3 output"""
+
+    p3_global = {
+        "PRIMER_NUM_RETURN": 10,
+        "PRIMER_PRODUCT_SIZE_RANGE": [
+            [config.AMPLICON_SIZE_MIN, config.AMPLICON_SIZE_MIN]
+        ],
+        "PRIMER_OPT_SIZE": config.PRIMER_OPT_SIZE,
+        "PRIMER_MIN_SIZE": config.PRIMER_SIZE_MIN,
+        "PRIMER_MAX_SIZE": config.PRIMER_SIZE_MAX,
+        "PRIMER_OPT_TM": config.PRIMER_OPT_TM,
+        "PRIMER_MIN_TM": config.PRIMER_MIN_TM,
+        "PRIMER_MAX_TM": config.PRIMER_MAX_TM,
+        "PRIMER_MIN_GC": config.PRIMER_MIN_GC,
+        "PRIMER_MAX_GC": config.PRIMER_MAX_GC,
+        "PRIMER_MAX_POLY_X": config.PRIMER_MAX_HOMO,
+        "PRIMER_SALT_MONOVALENT": config.MV_CONC,
+    }
 
     p3_seq = {
         "SEQUENCE_TEMPLATE": random_reference_slice,
@@ -37,7 +30,7 @@ def test_base_penalty_matches_primer3_penalty(random_reference_slice):
         "SEQUENCE_INCLUDED_REGION": [-1, -1],
     }
 
-    p3_output = primer3.bindings.designPrimers(p3_seq, P3_GLOBAL)
+    p3_output = primer3.bindings.designPrimers(p3_seq, p3_global)
     text_dir = ("LEFT", "RIGHT")
 
     for d in range(2):
