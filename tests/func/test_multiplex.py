dde58a2729007673fc90a9dbd1ebc30b378336dd
Andrew Smith <andysmith.d@gmail.com>
2020-06-12 18:27:25 +0100

    Update candidate sorting tests

--- tests/func/test_multiplex.py
+++ tests/func/test_multiplex.py
@@ -1,12 +1,11 @@
 import pytest
 
-from primalscheme.align import align_primer
 from primalscheme.cli import process_fasta
 from primalscheme.multiplex import MultiplexScheme
 
 
 @pytest.fixture(scope="session")
-def default_chikv_scheme(chikv_input):
+def chikv_scheme(chikv_input):
     return get_scheme(chikv_input)
 
 
@@ -34,8 +33,8 @@ def no_collisions(regions):
     return True
 
 
-def test_chikv_scheme_has_no_gaps(default_chikv_scheme):
-    regions = default_chikv_scheme.regions
+def test_chikv_scheme_has_no_gaps(chikv_scheme):
+    regions = chikv_scheme.regions
     inserts = [(r.left.end + 1, r.right.end) for r in regions]
     covered_coords = set([x for insert in inserts for x in range(*insert)])
     all_coords = set(range(regions[0].left.end + 1, regions[-1].right.end))
@@ -72,50 +71,73 @@ def test_large_target_overlap_does_not_result_in_collision(chikv_input):
     assert no_collisions(scheme.regions)
 
 
-def test_left_candidates_are_correctly_sorted(default_chikv_scheme):
-    region = default_chikv_scheme.regions[0]
-    penalty = region.left.combined_penalty
-    for candidate in region.left_candidates:
-        if candidate.combined_penalty < penalty:
-            pytest.fail("Candidates are not sorted by combined penalty")
-        penalty = candidate.combined_penalty
+def test_candidates_are_sorted_first_by_penalty(chikv_scheme):
+    regions = chikv_scheme.regions
+    for region in regions:
+        candidates = region.left_candidates + region.right_candidates
+        prev = None
+        direction = "LEFT"
+        for candidate in candidates:
+            if candidate.direction.name != direction:
+                direction = "RIGHT"
+                prev = None
+            if prev:
+                if candidate.combined_penalty < prev:
+                    pytest.fail("Candidates are not sorted first by combined_penalty.")
+            prev = candidate.combined_penalty
 
 
-def test_right_candidates_are_correctly_sorted(default_chikv_scheme):
-    region = default_chikv_scheme.regions[0]
-    penalty = region.right.combined_penalty
-    for candidate in region.right_candidates:
-        if candidate.combined_penalty < penalty:
-            pytest.fail("Candidates are not sorted by combined penalty")
-        penalty = candidate.combined_penalty
+def test_candidates_are_sorted_second_by_start(chikv_scheme):
+    regions = chikv_scheme.regions
+    for region in regions:
+        candidates = region.left_candidates + region.right_candidates
+        prev = None
+        direction = "LEFT"
+        for candidate in candidates:
+            attrs = (candidate.combined_penalty, candidate.start)
+            if candidate.direction.name != direction:
+                direction = "RIGHT"
+                prev = None
+            if prev:
+                if attrs[0] == prev[0] and attrs[1] > prev[1]:
+                    pytest.fail("Candidates are not sorted second by start position.")
+            prev = attrs
 
 
-def test_left_primer_seq_matches_some_ref_slice(default_chikv_scheme):
-    left = default_chikv_scheme.regions[0].left
+def test_candidates_are_sorted_third_by_seq(ebola_scheme):
+    regions = ebola_scheme.regions
+    for region in regions:
+        candidates = region.left_candidates + region.right_candidates
+        prev = None
+        direction = "LEFT"
+        for candidate in candidates:
+            attrs = (candidate.combined_penalty, candidate.start, candidate.seq)
+            if candidate.direction.name != direction:
+                direction = "RIGHT"
+                prev = None
+            if prev:
+                if attrs[0] == prev[0] and attrs[1] == prev[1] and attrs[2] < prev[2]:
+                    pytest.fail("Candidates are not sorted third by seq.")
+            prev = attrs
+
+
+def test_left_primer_seq_matches_some_ref_slice(chikv_scheme):
+    left = chikv_scheme.regions[0].left
     ref_slices = left.reference_msa
 
     assert any(ref_slice.seq == left.seq for ref_slice in ref_slices)
 
 
-def test_right_primer_seq_matches_some_ref_slice(default_chikv_scheme):
-    right = default_chikv_scheme.regions[0].right
+def test_right_primer_seq_matches_some_ref_slice(chikv_scheme):
+    right = chikv_scheme.regions[0].right
     ref_slices = right.reference_msa
 
     assert any(ref_slice.seq == right.seq for ref_slice in ref_slices)
 
 
-def test_scheme_with_single_reference(chikv_input):
+def test_scheme_runs_with_single_reference(chikv_input):
     references = process_fasta(chikv_input)[:1]
     scheme = MultiplexScheme(references)
     scheme.design_scheme()
 
-    assert len(scheme.regions) > 0
-
-
-def test_inferred_mismatch_alignments(ebola_scheme):
-    scheme = ebola_scheme
-    primers = scheme.primers
-    for primer in primers:
-        mismatch_counts = [align_primer(primer, ref)[0] for ref in scheme.references]
-        print(primer.reference_msa)
-        assert primer.mismatch_counts == mismatch_counts
+    assert len(scheme.regions) > 30
